9bad127ba2a69c31a160413b09ad317be1745f23
Nianjun Sun <sunnianjun@sphere-ex.com>
2022-02-21 21:10:40 +0800

    Add : add new test case

---
+++ src/main/java/com/github/shardingsphere/paper/cornucopia/jdbctest/cases/MergeSortRange.java
@@ -0,0 +1,35 @@
+package com.github.shardingsphere.paper.cornucopia.jdbctest.cases;
+
+import com.github.shardingsphere.paper.cornucopia.jdbctest.constants.SysbenchConstant;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MergeSortRange implements SysbenchBenchmark{
+    
+    private final Connection connection;
+    
+    private final PreparedStatement mergeRangeStatement;
+    
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+    
+    public MergeSortRange(Connection connection) throws SQLException {
+        this.connection = connection;
+        String sql = "SELECT c FROM sbtest1 WHERE ? > 0 AND id BETWEEN 1 AND 100";
+        mergeRangeStatement = connection.prepareStatement(sql);
+        if ("mysql".equalsIgnoreCase(SysbenchConstant.dbType)) {
+            mergeRangeStatement.setFetchSize(Integer.MIN_VALUE);
+        } else if ("pgsql".equalsIgnoreCase(SysbenchConstant.dbType)){
+            mergeRangeStatement.setFetchSize(1);
+        }
+    }
+    
+    @Override
+    public void execute() throws SQLException {
+        int id = random.nextInt(SysbenchConstant.tableSize);
+        mergeRangeStatement.setInt(1, id);
+        mergeRangeStatement.execute();
+    }
+}
