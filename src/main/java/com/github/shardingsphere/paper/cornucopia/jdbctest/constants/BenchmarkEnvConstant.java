526d7c331becee589176526073ee486aa88debae
Nianjun Sun <sunnianjun@sphere-ex.com>
2022-02-24 08:16:39 +0800

    Refactor : refactor the environment variables

--- src/main/java/com/github/shardingsphere/paper/cornucopia/jdbctest/constants/BenchmarkEnvConstant.java
+++ src/main/java/com/github/shardingsphere/paper/cornucopia/jdbctest/constants/BenchmarkEnvConstant.java
@@ -1,6 +1,6 @@
 package com.github.shardingsphere.paper.cornucopia.jdbctest.constants;
 
-public class SysbenchConstant {
+public class BenchmarkEnvConstant {
     
     public static String jdbcType = "ss-jdbc";
     
@@ -43,39 +43,36 @@ public class SysbenchConstant {
         tableSize = Integer.parseInt(System.getProperty("table-size"));
         tables = Integer.parseInt(System.getProperty("tables"));
         distributionTransaction = System.getProperty("transaction-mode");
-        if (null == System.getProperty("percentile") || "".equals(System.getProperty("percentile"))){
-            percentile = 99;
-        } else {
-            percentile = Integer.parseInt(System.getProperty("percentile"));
-        }
-        if (null != System.getProperty("jdbc-type")) {
-            jdbcType = System.getProperty("jdbc-type");
-        }
-        if (null == System.getProperty("jdbc-type")) {
-            jdbcType = "ss-jdbc";
-        }
-        if (null != System.getProperty("jdbc-url")) {
-            jdbcUrl = System.getProperty("jdbc-url");
-        }
-        if (null != System.getProperty("username")) {
-            username = System.getProperty("username");
-        }
-        if (null != System.getProperty("password")) {
-            password = System.getProperty("password");
-        }
-        if (null != System.getProperty("merge-sort-from")) {
-            mergeSortFrom = Integer.parseInt(System.getProperty("merge-sort-from"));
-        }
-        if (null != System.getProperty("merge-sort-to")) {
-            mergeSortTo = Integer.parseInt(System.getProperty("merge-sort-to"));
-        }
+        percentile = getIntProperty("percentile", 99);
+        jdbcType = getStringProperty("jdbc-type", "ss-jdbc");
+        jdbcUrl = getStringProperty("jdbc-url", null);
+        username = getStringProperty("username", null);
+        password = getStringProperty("password", null);
+        mergeSortFrom = getIntProperty("merge-sort-from", 0);
+        mergeSortTo = getIntProperty("merge-sort-to", 0);
+        dbType = getStringProperty("db-type", "mysql");
         if (null != System.getProperty("use-transaction")) {
             useTransaction = Boolean.parseBoolean(System.getProperty("use-transaction"));
         }
-        if (null != System.getProperty("db-type")) {
-            dbType = System.getProperty("db-type");
+    }
+    
+    private static String getStringProperty(String key, String defaultProperty) {
+        String property = System.getProperty(key);
+        String result; 
+        if (null != property) {
+            result = property;
         } else {
-            dbType = "mysql";
-        } 
+            result = defaultProperty;
+        }
+        return result;
+    }
+    
+    private static int getIntProperty(String key, int defaultValue) {
+        String property = System.getProperty(key);
+        int result = defaultValue;
+        if (null != property) {
+            result = Integer.parseInt(property);
+        }
+        return result;
     }
 }
